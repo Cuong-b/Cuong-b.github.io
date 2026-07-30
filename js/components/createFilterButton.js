88cfdd628364a2a3fd696839e468a56029cca349
Cuong <cb626@berkeley.edu>
2026-07-29 17:06:54 -0700

    feat: filter button takes into account active status. Turns filter off on click if active.

--- js/components/createFilterButton.js
+++ js/components/createFilterButton.js
@@ -4,9 +4,16 @@ import { render } from "../render";
 
 export function createFilterButton(filter){
     const onClick = () => {
+        if(filter.active){
+            state.selectedCategory = "All"
+            filter.active = false;
+            render();
+        }
+        else {
         state.selectedCategory = filter.label;
         filter.active = true;
         render();
+        }
     };
     const button = createButton(filter.label, onClick)
     return button;
