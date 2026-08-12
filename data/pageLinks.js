ce997e4d2952014a90d6e33cbbfff89855f30736
Cuong <cb626@berkeley.edu>
2026-08-12 14:56:15 -0700

    Fixing navbar links. Removed /pages/.

--- data/pageLinks.js
+++ data/pageLinks.js
@@ -1,7 +1,7 @@
 export const links = [
-        { href: '/pages/index.html', text: 'Home' },
-        { href: '/pages/about.html', text: 'About' },
-        { href: '/pages/projects.html', text: 'Projects' },
-        { href: '/pages/resume.html', text: 'Resume' }, 
-        { href: '/pages/contact.html', text: 'Contact' }
+        { href: 'index.html', text: 'Home' },
+        { href: 'about.html', text: 'About' },
+        { href: 'projects.html', text: 'Projects' },
+        { href: 'resume.html', text: 'Resume' }, 
+        { href: 'contact.html', text: 'Contact' }
     ];
